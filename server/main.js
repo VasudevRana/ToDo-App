82565d6a8c9760ef543293b4ad4aa1d867112d87
VasudevRana <45121999+VasudevRana@users.noreply.github.com>
2020-09-08 15:38:48 +0530

    Update main.js

--- server/main.js
+++ server/main.js
@@ -1,22 +1,62 @@
 import { Meteor } from 'meteor/meteor';
 Resolutions = new Mongo.Collection("resolutions");
 
+
+
+
+
+
+
 Meteor.startup(() => {
   // code to run on server at startup
+
+
+  Meteor.publish("resolution", function () {
+    return Resolutions.find({
+      $or:
+        [{ private: { $ne: true } },
+        { owner: this.userId }]
+    });
+  })
+
+
   Meteor.methods({
     addResolution: function (title) {
       Resolutions.insert({
         title: title,
-        createdAt: new Date()
+        createdAt: new Date(),
+        owner: Meteor.userId()
       });
     }
     ,
     update: function (id, checked) {
+      var res = Resolutions.findOne(id);
+
+      if (res.owner !== Meteor.userId()) {
+        throw new Meteor.Error('not-authorized');
+      }
       Resolutions.update(id, { $set: { checked: checked } })
     }
     ,
     delete: function (id) {
+      var res = Resolutions.findOne(id);
+
+      if (res.owner !== Meteor.userId()) {
+        throw new Meteor.Error('not-authorized');
+      }
       Resolutions.remove(id);
+
+    },
+    setPrivate: function (id, private) {
+      var res = Resolutions.findOne(id);
+
+      if (res.owner !== Meteor.userId()) {
+        throw new Meteor.Error('not-authorized');
+      }
+      Resolutions.update(id, { $set: { private: private } })
+
+
+
 
     }
   })
